17288cf3853157cc71da33b7d788f2e4d1348be1
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-20 19:03:09 -0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,21 @@
+
+struct mySchool:
+
+
+func myBackpack(
+	carryHeavy = ["Heavy Duty Backpack","Hiking Backpack","Luggage Bag","Duffel Bag"]
+	carryLight = ["Carrier bag", "Slim Laptop Bag","Clear Backpack"]
+)
+
+func myNotebook(
+	myBinder = input()
+	regularItems = ["Pencil case","pencils","pens","paper","notebook", "binder"]
+
+)
+
+func myTechnology(
+	myReader = ["Apple iPad (Pro, Air, Mini, Regular)","Amazon Kindle"]
+	if myBinder != "notebook":
+	print("Your student should substitute that with one of these items" + myReader + ".")
+	myLaptop = ["Chromebook","HP Inspiron","Apple Laptop"]
+)
